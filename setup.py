8f70eae7a6b2115b636c96dd2ad851272432bdf1
unknown <ragedshazam2002@gmail.com>
2025-02-23 23:27:06 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Parshuram Das',
+    author_email= 'dasparshu74@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
